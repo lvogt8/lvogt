5c2b9905d0a8718bf8d5a1ca87b96064e35952b8
lvogt8 <54590468+lvogt8@users.noreply.github.com>
2020-01-16 09:49:24 -0600

    Initial commit

---
+++ CSCE/NewCSCE/Hack/hello/hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Landon Vogt
+ * Date: 1/16/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Landon Vogt\n");
+
+
+  return 0;
+}
